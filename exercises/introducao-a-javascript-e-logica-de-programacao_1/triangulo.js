b5546580ca60145125b2b4ebb711d97d8ab2d0b0
tiagaoalb <tiagaoalb@gmail.com>
2020-10-23 19:15:42 -0300

    added xadrez

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/triangulo.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/triangulo.js
@@ -1,10 +1,10 @@
 const x = 20;
 const y = 30;
-const z = 110;
+const z = 130;
 
 const result = x + y + z;
 
-if (typeof result != "number") {
+if (typeof result !== "number") {
   console.error("Valor inválido!");
 } else if (result === 180) {
   console.log(true);
---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/xadrez.js
@@ -0,0 +1,19 @@
+const peça = "bispo";
+
+if (typeof peça !== "string") {
+  console.error("Entrada inválida!");
+} else if (peça.toLowerCase() === "rainha") {
+  console.log("Pode se mover na horizontal, vertical e diagonais uma casa de cada vez.");
+} else if (peça.toLowerCase() === "rei") {
+  console.log("Pode se mover na horizontal, vertical e diagonais, sem pular outras peças.");
+} else if (peça.toLowerCase() === "bispo") {
+  console.log("Pode se mover na diagonal.");
+} else if (peça.toLowerCase() === "cavalo") {
+  console.log("Pode se mover em L e pular outras peças.");
+} else if (peça.toLowerCase() === "torre") {
+  console.log("Pode se mover na vertical ou horizontal, mas naão pode pular outras peças.");
+} else if (peça.toLowerCase() === "peao") {
+  console.log("Pode se mover apenas uma casa e somente captura outras peças na diagonal.");
+} else {
+  console.log("Peça inválida!");
+}
