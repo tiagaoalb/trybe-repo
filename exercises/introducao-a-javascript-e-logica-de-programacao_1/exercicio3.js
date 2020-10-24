a5548c2bb1a9cb31b233160d9eef9aff2d91c041
tiagaoalb <tiagaoalb@gmail.com>
2020-10-24 12:42:14 -0300

    added a new line at the end of some files

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio3.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio3.js
@@ -2,10 +2,10 @@ const x = 10;
 const y = 15;
 const z = 25;
 
-if (x > y & x > z) {
-  console.log(x)
-} else if (y > x & y > z) {
-  console.log(y)
+if ((x > y) & (x > z)) {
+  console.log(x);
+} else if ((y > x) & (y > z)) {
+  console.log(y);
 } else {
-  console.log(z)
+  console.log(z);
 }
