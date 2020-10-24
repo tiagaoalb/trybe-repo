6c4357a5d56a8c3de4eec7d97e8aebbd5814361f
tiagaoalb <tiagaoalb@gmail.com>
2020-10-24 12:42:14 -0300

    added a new line at the of some files

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio7.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio7.js
@@ -1,17 +1,17 @@
 const nota = 89;
 
 if (nota < 0 || nota > 100) {
-  console.error("Valor da nota inválido!")
+  console.error("Valor da nota inválido!");
 } else if (nota >= 90) {
-  console.log("A")
+  console.log("A");
 } else if (nota >= 80) {
-  console.log("B")
+  console.log("B");
 } else if (nota >= 70) {
-  console.log("C")
+  console.log("C");
 } else if (nota >= 60) {
-  console.log("D")
+  console.log("D");
 } else if (nota >= 50) {
-  console.log("E")
+  console.log("E");
 } else {
-  console.log("F")
+  console.log("F");
 }
