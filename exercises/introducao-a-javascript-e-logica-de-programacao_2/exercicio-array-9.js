38a35bf2c03eb74808eb85d881f50674752b2f26
tiagaoalb <tiagaoalb@gmail.com>
2020-10-26 16:04:43 -0300

    updated exercicio array 9

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio-array-9.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio-array-9.js
@@ -1,10 +1,9 @@
 let array25 = [];
+let division = [];
 
 for (let i = 1; i < 26; i++) {
   array25.push(i);
 }
-
-let division = [];
 
 for (let div in array25) {
   division.push(array25[div] / 2);
