acddbc52dd0863c89eaccaab1ac4d340bd57a438
tiagaoalb <tiagaoalb@gmail.com>
2020-10-27 13:07:40 -0300

    updated logica exercicio 1

--- exercises/introducao-a-javascript-e-logica-de-programacao_3/logica-exercicio-1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/logica-exercicio-1.js
@@ -1,7 +1,5 @@
 let number = 4;
-let asterisk = "";
 
-for (let altura = 0; altura < number; altura += 1) {
-  asterisk = "*";
-  console.log(asterisk);
+for (altura = 1; altura <= number; altura += 1) {
+  
 }
