02ac7c0ea797e262e31793eea054690dd817324a
Tiago Lunardi <tiagaoalb@gmail.com>
2020-11-08 22:32:05 -0300

    updated index and script

---
+++ exercises/javascript-dom-eventos-e-web-storage_4/script.js
@@ -0,0 +1,39 @@
+const paragraphText = document.querySelectorAll('.paragraph');
+
+let changeBackgroundColor = (color) => {
+  let colorOption = color.options[color.selectedIndex].value;
+  document.body.style.backgroundColor = colorOption;
+  /* localStorage.setItem('backgroundColor', color); */
+};
+
+let changeFontColor = (color) => {
+  let textColorOption = color.options[color.selectedIndex].value;
+  for (let i of paragraphText) {
+    i.style.color = textColorOption;
+  }
+  /* localStorage.setItem('fontColor', color); */
+};
+
+let changeFontSize = (size) => {
+  let textSize = size.options[size.selectedIndex].value;
+  for (let i of paragraphText) {
+    i.style.fontSize = textSize;
+  }
+  /* localStorage.setItem('fontSize', size); */
+}
+
+let changeLineHeight = (height) => {
+  let lineHeight = height.options[height.selectedIndex].value;
+  for (let i of paragraphText) {
+    i.style.lineHeight = lineHeight;
+  }
+  /* localStorage.setItem('lineHeight', height); */
+}
+
+let changeLineHeight = (height) => {
+  let lineHeight = height.options[height.selectedIndex].value;
+  for (let i of paragraphText) {
+    i.style.lineHeight = lineHeight;
+  }
+  /* localStorage.setItem('lineHeight', height); */
+}
