51abc0e498379c8a1266ee778bf9d7f220eb6687
Tiago Lunardi <tiagaoalb@gmail.com>
2020-11-12 12:30:35 -0300

    updated script and index

--- exercises/javascript-dom-eventos-e-web-storage_4/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_4/script.js
@@ -1,39 +1,64 @@
 const paragraphText = document.querySelectorAll('.paragraph');
 
+// Change background color
+let getSelectBG = document.querySelector('#background-color');
+getSelectBG.addEventListener('change', function (event) {
+  changeBackgroundColor(event.target.value);
+});
+
 let changeBackgroundColor = (color) => {
-  let colorOption = color.options[color.selectedIndex].value;
-  document.body.style.backgroundColor = colorOption;
+  document.body.style.backgroundColor = color;
   localStorage.setItem('backgroundColor', color);
 };
 
+// Change font color
+let getSelectFC = document.querySelector('#text-color');
+getSelectFC.addEventListener('change', function (event) {
+  changeFontColor(event.target.value);
+});
+
 let changeFontColor = (color) => {
-  let textColorOption = color.options[color.selectedIndex].value;
   for (let i of paragraphText) {
-    i.style.color = textColorOption;
+    i.style.color = color;
   }
   localStorage.setItem('fontColor', color);
 };
 
+// Change font size
+let getSelectFS = document.querySelector('#font-size');
+getSelectFS.addEventListener('change', function (event) {
+  changeFontSize(event.target.value);
+});
+
 let changeFontSize = (size) => {
-  let textSize = size.options[size.selectedIndex].value;
   for (let i of paragraphText) {
-    i.style.fontSize = textSize;
+    i.style.fontSize = size;
   }
   localStorage.setItem('fontSize', size);
 };
 
+// Change line height
+let getSelectLH = document.querySelector('#line-height');
+getSelectLH.addEventListener('change', function (event) {
+  changeLineHeight(event.target.value);
+});
+
 let changeLineHeight = (height) => {
-  let lineHeight = height.options[height.selectedIndex].value;
   for (let i of paragraphText) {
-    i.style.lineHeight = lineHeight;
+    i.style.lineHeight = height;
   }
   localStorage.setItem('lineHeight', height);
 };
 
+// Change font family
+let getSelectFF = document.querySelector('#font-type');
+getSelectFF.addEventListener('change', function (event) {
+  changeFontFamily(event.target.value);
+});
+
 let changeFontFamily = (family) => {
-  let fontType = family.options[family.selectedIndex].value;
   for (let i of paragraphText) {
-    i.style.fontFamily = fontType;
+    i.style.fontFamily = family;
   }
   localStorage.setItem('fontFamily', family);
 };
