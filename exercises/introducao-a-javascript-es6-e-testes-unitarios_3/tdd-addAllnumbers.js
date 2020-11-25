da3517cb1c0c4b69e2799ad304846630e57c1503
Tiago Lunardi <tiagaoalb@gmail.com>
2020-11-25 19:15:00 -0300

    added tdd-findTheNeedle

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_3/tdd-addAllnumbers.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/tdd-addAllnumbers.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 
-const addAllnumbers = (numbers) => {
+const addAllNumbers = (numbers) => {
   let output = 0;
   for (let index = 0; index < numbers.length; index += 1) {
     output += numbers[index];
@@ -10,7 +10,7 @@ const addAllnumbers = (numbers) => {
 
 const numbers = [9, 23, 10, 3, 8];
 const expected = 53;
-const output = addAllnumbers(numbers);
+const output = addAllNumbers(numbers);
 
-assert.strictEqual(typeof addAllnumbers, 'function');
+assert.strictEqual(typeof addAllNumbers, 'function');
 assert.strictEqual(output, expected);
