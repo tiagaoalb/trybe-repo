bd044739ca051d8001c30d3a77bcccef99afff2b
Tiago Lunardi <tiagaoalb@gmail.com>
2020-11-24 22:00:45 -0300

    added longesWord

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/factorial.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/factorial.js
@@ -1,8 +1,13 @@
-const factorial = (number) => {
+/* const factorial = (number) => {
   let result = 1;
   for (let index = 1; index <= number; index += 1) {
     result *= index;
   }
   return result;
+};
+console.log(factorial(5)); */
+
+const factorial = (n) => {
+  return n >= 1 ? n * factorial(n - 1) : 1;
 };
 console.log(factorial(5));
