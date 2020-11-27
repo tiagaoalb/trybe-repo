dc021f3900bfa18a6484a1f4107f34822343c471
Tiago Lunardi <tiagaoalb@gmail.com>
2020-11-27 16:50:35 -0300

    updated sumTest

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_3/sumTest.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/sumTest.js
@@ -12,8 +12,8 @@ const expected = sum(4, '5');
 const expectedZero = sum(0, 0);
 
 assert.strictEqual(typeof sum, 'function');
-assert.strictEqual(expected, 9, 'A soma de 5 + 4 deve ser igual a 9.');
-assert.strictEqual(expectedZero, 0, 'A soma de 0 + 0 deve ser igual a 0.');
+assert.strictEqual(expected, 9);
+assert.strictEqual(expectedZero, 0);
 assert.throws(() => {
   sum(4, '5');
 }, /^Error: parameters must be numbers$/);
