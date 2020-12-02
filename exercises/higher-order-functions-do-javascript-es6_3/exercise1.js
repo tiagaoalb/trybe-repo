997e1cce4a38313e5b109818ba0e4fb34bb8bfa8
Tiago Auguto Lunardi Bovolin <tiagaoalb@pop-os.localdomain>
2020-12-02 15:33:12 -0300

    added exercise1

---
+++ exercises/higher-order-functions-do-javascript-es6_3/exercise1.js
@@ -0,0 +1,9 @@
+const assert = require("assert");
+
+const arrays = [["1", "2", "3"], [true], [4, 5, 6]];
+
+function flatten() {
+  return arrays.reduce((element1, element2) => element1.concat(element2));
+}
+
+assert.deepStrictEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
