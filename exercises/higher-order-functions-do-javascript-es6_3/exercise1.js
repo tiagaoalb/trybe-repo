e0e5f524c642fd3b679b9a5372ddcae7ea5487e7
Tiago Lunardi <tiagaoalb@gmail.com>
2020-12-02 21:05:02 -0300

    added exercise3

--- exercises/higher-order-functions-do-javascript-es6_3/exercise1.js
+++ exercises/higher-order-functions-do-javascript-es6_3/exercise1.js
@@ -3,7 +3,7 @@ const assert = require("assert");
 const arrays = [["1", "2", "3"], [true], [4, 5, 6]];
 
 function flatten() {
-  return arrays.reduce((element1, element2) => element1.concat(element2));
+  return arrays.reduce((element1, element2) => element1.concat(element2), []);
 }
 
 assert.deepStrictEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
