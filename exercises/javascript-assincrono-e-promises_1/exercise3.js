c195aacc6e438107ecb18eb392103499b7a68d91
Tiago <tiagaoalb@gmail.com>
2020-12-09 11:12:17 -0300

    added exercise4

--- exercises/javascript-assincrono-e-promises_1/exercise3.js
+++ exercises/javascript-assincrono-e-promises_1/exercise3.js
@@ -1,14 +1,12 @@
 const getPlanet = () => {
-  setTimeout(() => {
-    const mars = {
-      name: 'Mars',
-      distanceFromSun: {
-        value: 227900000,
-        measurementUnit: 'kilometers',
-      },
-    };
-    console.log('Returned planet: ', mars);
-  }, 4000);
+  const mars = {
+    name: 'Mars',
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: 'kilometers',
+    },
+  };
+  setTimeout(() => console.log('Returned planet: ', mars), 4000);
 };
 
 getPlanet(); // imprime Marte depois de 4 segundos
