5299f9435da7c1a39022e18058a72d259edd31ff
Tiago <tiagaoalb@gmail.com>
2021-01-02 15:01:41 -0300

    added 5th exercise

---
+++ exercises/testes-automatizados-com-jest_1/jest-exercise-5.test.js
@@ -0,0 +1,14 @@
+const obj1 = {
+  title: 'My Title',
+  description: 'My Description',
+};
+
+const obj2 = {
+  description: 'My Description',
+  title: 'My Title',
+};
+
+const obj3 = {
+  title: 'My Different Title',
+  description: 'My Description',
+};
