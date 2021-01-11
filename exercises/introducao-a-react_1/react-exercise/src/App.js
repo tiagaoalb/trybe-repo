f1f237e93f02657b07bfb46ff3c2810fb8fb932b
Tiago <tiagaoalb@gmail.com>
2021-01-11 11:19:05 -0300

    created task list

--- exercises/introducao-a-react_1/react-exercise/src/App.js
+++ exercises/introducao-a-react_1/react-exercise/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+const taskArray = ['React', 'Angular', 'Vue', 'Redux']
+const Task = () => {
+  return <ul>List of technologies{taskArray.map(i => <li>{i}</li>)}</ul>;
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Task />
   );
 }
 
