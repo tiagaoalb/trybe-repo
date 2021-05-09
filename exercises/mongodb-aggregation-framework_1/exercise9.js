fdfdb3195d1297d47cc75c62e0a42b0894fdf37f
Tiago <tiagaoalb@gmail.com>
2021-05-09 14:23:12 -0300

    add 9th exercise

---
+++ exercises/mongodb-aggregation-framework_1/exercise9.js
@@ -0,0 +1,10 @@
+db.vendas.aggregate([
+  {
+    $match: {
+      dataVenda: { $gte: ISODate('2019-01-01'), $lte: ISODate('2019-12-31') },
+    },
+  },
+  { $group: { _id: '$clienteId', valorTotal: { $sum: '$valorTotal' } } },
+  { $sort: { valorTotal: -1 } },
+  { $limit: 10 },
+]);
