8a31d83c28af55ae4223292a5f35b768d2c01b84
Tiago <tiagaoalb@gmail.com>
2021-05-09 21:29:02 -0300

    change name from totalClientes to totalVendas

--- exercises/mongodb-aggregation-framework_1/exercise11.js
+++ exercises/mongodb-aggregation-framework_1/exercise11.js
@@ -4,7 +4,7 @@ db.vendas.aggregate([
       dataVenda: { $gte: ISODate('2020-01-01'), $lte: ISODate('2020-03-01') },
     },
   },
-  { $group: { _id: '$clienteId', totalClientes: { $sum: 1 } } },
-  { $match: { totalClientes: { $lt: 3 } } },
+  { $group: { _id: '$clienteId', totalVendas: { $sum: 1 } } },
+  { $match: { totalVendas: { $lt: 3 } } },
   { $count: 'clientes' },
 ]);
