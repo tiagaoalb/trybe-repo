49cba39eb7ba459f3e13372ff7f4ec1b038f5e48
Tiago <tiagaoalb@gmail.com>
2021-05-13 19:04:54 -0300

    add velocidade

--- exercises/introducao-ao-desenvolvimento-web-com-nodejs_1/my-scripts/imc.js
+++ exercises/introducao-ao-desenvolvimento-web-com-nodejs_1/my-scripts/imc.js
@@ -1,15 +1,32 @@
-const WEIGHT_76KG = 76;
-const HEIGHT_1M_87CM = 1.87;
+const readline = require('readline-sync');
 
-const calculaImc = () => {
-  const weight = WEIGHT_76KG;
-  const height = HEIGHT_1M_87CM;
-  
+const ask_weight = readline.questionInt('What is your weight? ');
+const ask_height = readline.questionInt('What is your height? ');
+
+const imcCalc = (weight, height) => {
   console.log(`Weight ${weight} kg and height ${height} m`);
   
-  const imc = (weight / Math.pow(height, 2)).toFixed(2);
+  const imc = (weight / Math.pow((height / 100), 2)).toFixed(2);
   
   console.log(`IMC: ${imc}`);
+
+  if(imc < 18.5) {
+    console.log('Abaixo do peso (magreza)');
+    return;
+  } if (imc >= 18.5 && imc <= 24.9) {
+    console.log('Peso normal');
+    return;
+  } if (imc >= 25 && imc <= 29.9) {
+    console.log('Acima do peso (sobrepeso)');
+    return;
+  } if (imc >= 30 && imc <= 34.9) {
+    console.log('Obesidade grau I');
+    return;
+  } if (imc >= 35 && imc <= 39.9) {
+    console.log('Obesidade grau II');
+    return;
+  } 
+  console.log('Obesidades graus III e IV');
 }
 
-calculaImc();
+imcCalc(ask_weight, ask_height);
