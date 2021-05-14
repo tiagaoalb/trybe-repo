2510d1245048808e3e62da45d2e4502773cd8f7b
Tiago <tiagaoalb@gmail.com>
2021-05-14 18:57:19 -0300

    update selection script

--- exercises/introducao-ao-desenvolvimento-web-com-nodejs_1/my-scripts/index.js
+++ exercises/introducao-ao-desenvolvimento-web-com-nodejs_1/my-scripts/index.js
@@ -1,17 +1,18 @@
 const readline = require('readline-sync');
 
 const scripts = [
-  { name: 'Calcular IMC', script: './imc.js' },
-  { name: 'Calcular velocidade média', script: './velocidade.js' },
-  { name: 'Jogo de adivinhação', script: './sorteio.js' },
+  { name: 'Calcular IMC ', script: './imc.js' },
+  { name: 'Calcular velocidade média ', script: './velocidade.js' },
+  { name: 'Jogo de adivinhação ', script: './sorteio.js' },
 ];
 
-const msg = scripts.map((script, index) => `${index + 1} - ${script.name}`).join('\n');
+const msg = scripts
+  .map((script, index) => `${index + 1} - ${script.name}`)
 
-
-const concatMsg = `Escolha um número para executar o script correspondente: \n\n${msg}`;
-
-const scriptNumber = readline.questionInt(concatMsg) - 1;
+msg.unshift('Escolha um número para executar o script correspondente');
+msg.push('Digite a opção: ');
+const result = msg.join('\n')
+const scriptNumber = readline.questionInt(result) - 1;
 
 const script = scripts[scriptNumber];
 
