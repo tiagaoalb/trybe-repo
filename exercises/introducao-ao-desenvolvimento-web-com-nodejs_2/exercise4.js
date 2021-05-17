211034b223a59aa7b28c1b3e5c5cd7ea35424a15
Tiago <tiagaoalb@gmail.com>
2021-05-16 23:30:59 -0300

    updates 4th exercise

--- exercises/introducao-ao-desenvolvimento-web-com-nodejs_2/exercise4.js
+++ exercises/introducao-ao-desenvolvimento-web-com-nodejs_2/exercise4.js
@@ -1,13 +1,9 @@
 const fs = require('fs/promises');
 
 const readFile = async() => {
-  const getJson = await fs.readFile('simpsons.json', 'utf8')
-    .then((data) => {
-      return JSON.parse(data);
-    })
-    .then((simpsons) => {
-      return simpsons.map(({ id, name }) => `${id} - ${name}`);
-    })
+  const getJson = await fs.readFile('./simpsons.json', 'utf8')
+    .then((data) => JSON.parse(data))
+    .then((simpsons) => simpsons.map(({ id, name }) => `${id} - ${name}`))
     .then((strings) => {
       strings.forEach((string) => console.log(string));
     });
@@ -15,3 +11,14 @@ const readFile = async() => {
 }
 
 readFile();
+
+const getSimpsonById = async(id) => {
+  const getJson = await fs.readFile('./simpsons.json', 'utf8')
+    .then((data) => JSON.parse(data));
+
+  const findSimpson = getJson.find((simpson) => simpson.id === id);
+  if (!findSimpson) throw new Error('Id não encontrado!');
+  return console.log(findSimpson);
+}
+
+getSimpsonById('5');
