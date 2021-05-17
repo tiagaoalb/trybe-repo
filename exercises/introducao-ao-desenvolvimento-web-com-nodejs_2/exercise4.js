4ff385dd491523583b4e507547c124e8ee7c3c18
Tiago <tiagaoalb@gmail.com>
2021-05-16 23:06:18 -0300

    updates 4th exercise

--- exercises/introducao-ao-desenvolvimento-web-com-nodejs_2/exercise4.js
+++ exercises/introducao-ao-desenvolvimento-web-com-nodejs_2/exercise4.js
@@ -1,10 +1,17 @@
 const fs = require('fs/promises');
 
 const readFile = async() => {
-  const getJson = await fs.readFile('simpsons.json', 'utf8');
-  const setJson = JSON.parse(getJson);
-  const result = setJson.map(({ id, name }) => `${id} - ${name}`);
-  return console.log(result);
+  const getJson = await fs.readFile('simpsons.json', 'utf8')
+    .then((data) => {
+      return JSON.parse(data);
+    })
+    .then((simpsons) => {
+      return simpsons.map(({ id, name }) => `${id} - ${name}`);
+    })
+    .then((strings) => {
+      strings.forEach((string) => console.log(string));
+    });
+  return getJson;
 }
 
 readFile();
